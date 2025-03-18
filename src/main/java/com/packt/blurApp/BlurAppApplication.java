2ab1c27241a6958c2aff070adbb379e3a1c98334
TIOKOU Jake Melvin <138882910+jakemelvin@users.noreply.github.com>
2025-03-18 14:22:08 +0100

    chore: configure project

---
+++ src/main/java/com/packt/blurApp/BlurAppApplication.java
@@ -0,0 +1,13 @@
+package com.packt.blurApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlurAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packt/blurApp/BlurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.blurApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
