7209515c2e048012ef8538e138250b8f209b46ba
TIOKOU Jake Melvin <138882910+jakemelvin@users.noreply.github.com>
2025-03-19 20:11:10 +0100

    fix: resolve permission model issues

--- src/main/java/com/packt/blurApp/model/Permission.java
+++ src/main/java/com/packt/blurApp/model/Permission.java
@@ -1,6 +1,9 @@
 package com.packt.blurApp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Permission {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private Boolean isActive;
