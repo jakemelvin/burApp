37c55cad7547b87773528138b2dc8fec17027e0d
TIOKOU Jake Melvin <138882910+jakemelvin@users.noreply.github.com>
2025-03-20 20:19:39 +0100

    fix: arrange the party controller

---
+++ src/main/java/com/packt/blurApp/dto/Party/PartyGetResponseDto.java
@@ -0,0 +1,20 @@
+package com.packt.blurApp.dto.Party;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.packt.blurApp.dto.Race.RaceResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PartyGetResponseDto {
+  private Long id;
+  private LocalDate datePlayed;
+  private Set<RaceResponseDto> racesPlayed = new HashSet<>();
+}
