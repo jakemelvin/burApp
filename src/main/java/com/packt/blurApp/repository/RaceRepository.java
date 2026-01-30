5bc8b6c5e3005612932444489b3042df705f6246
mrvin100 <vincentyoumssi@example.com>
2026-01-30 14:56:17 +0100

    fix: build errors

--- src/main/java/com/packt/blurApp/repository/RaceRepository.java
+++ src/main/java/com/packt/blurApp/repository/RaceRepository.java
@@ -3,6 +3,7 @@ package com.packt.blurApp.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
 import com.packt.blurApp.model.Race;
@@ -10,6 +11,19 @@ import com.packt.blurApp.model.enums.RaceStatus;
 
 @Repository
 public interface RaceRepository extends JpaRepository<Race, Long> {
+    @EntityGraph(attributePaths = {
+        "party",
+        "creator",
+        "scoreCollector",
+        "card",
+        "raceParameters",
+        "participants",
+        "scores",
+        "scores.user",
+        "attributions",
+        "attributions.user",
+        "attributions.car"
+    })
     List<Race> findByParty_Id(Long partyId);
     
     List<Race> findByPartyIdAndStatus(Long partyId, RaceStatus status);
